09696064e42cef5f3bb742fffece89b84ff80f81
KGreen <kevin.e.green@hotmail.com>
2018-06-17 15:53:20 -0500

    Added Bottom Navigation Menu

--- app/src/main/java/com/example/android/musicapp/InformationAdapter.java
+++ app/src/main/java/com/example/android/musicapp/InformationAdapter.java
@@ -18,22 +18,21 @@ public class InformationAdapter extends ArrayAdapter<Information> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Check if an existing view is being reused, otherwise inflate the view
-        View listItemView = convertView;
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
+        if (null == convertView) {
+            convertView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_view, parent, false);
         }
 
         Information currentWord = getItem(position);
 
-        TextView mainTextView = (TextView) listItemView.findViewById(R.id.main_text);
+        TextView mainTextView = (TextView) convertView.findViewById(R.id.main_text);
 
         mainTextView.setText(currentWord.getFirstLine());
 
-        TextView secondaryTextView = (TextView) listItemView.findViewById(R.id.secondary_text);
+        TextView secondaryTextView = (TextView) convertView.findViewById(R.id.secondary_text);
 
         secondaryTextView.setText(currentWord.getSecondLine());
 
-        return listItemView;
+        return convertView;
     }
 }
