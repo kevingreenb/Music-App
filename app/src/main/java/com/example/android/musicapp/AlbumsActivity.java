86f810c2de9ee8fa6ec5f0cbc1a3665dbd94265e
KGreen <kevin.e.green@hotmail.com>
2018-06-17 15:34:33 -0500

    Added Bottom Navigation Menu

--- app/src/main/java/com/example/android/musicapp/AlbumsActivity.java
+++ app/src/main/java/com/example/android/musicapp/AlbumsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.internal.BottomNavigationItemView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -34,5 +37,48 @@ public class AlbumsActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(adapter);
+
+        //Navigation Listeners. I am hoping to be reuse the code below, but I don't know how, please advice.
+
+        BottomNavigationItemView homeButton = (BottomNavigationItemView) findViewById(R.id.navigation_home);
+        BottomNavigationItemView artistsButton = (BottomNavigationItemView) findViewById(R.id.navigation_artists);
+        BottomNavigationItemView songsButton = (BottomNavigationItemView) findViewById(R.id.navigation_songs);
+
+        // Set a click listener
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link MainActivity}
+                Intent mainIntent = new Intent(AlbumsActivity.this, MainActivity.class);
+
+                // Start the new activity
+                startActivity(mainIntent);
+
+            }
+        });
+        // Set a click listener
+        artistsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link MainActivity}
+                Intent mainIntent = new Intent(AlbumsActivity.this, ArtistsActivity.class);
+
+                // Start the new activity
+                startActivity(mainIntent);
+
+            }
+        });
+        // Set a click listener
+        songsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link MainActivity}
+                Intent mainIntent = new Intent(AlbumsActivity.this, SongsActivity.class);
+
+                // Start the new activity
+                startActivity(mainIntent);
+
+            }
+        });
     }
 }
